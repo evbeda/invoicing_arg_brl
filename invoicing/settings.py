73e9507aa972782993ba972a9b37338bd109c291
German Hidalgo <ghidalgo-eb@eventbrite.com>
2020-05-06 16:57:01 -0300

    Adapted models and tests with QA DB schema

--- invoicing/settings.py
+++ invoicing/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'invoicing',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 3306,
     },
