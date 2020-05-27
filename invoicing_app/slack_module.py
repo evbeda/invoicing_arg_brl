3d195f675a6d510b86ec83161a1d8a7232953316
agutierrez-eb <62262314+agutierrez-eb@users.noreply.github.com>
2020-05-27 09:14:44 -0300

    Eda 1023 slack integration (#25)
    * Adding a new file for  production
    
    * Making a local integration with slack
    
    * Fixing the slack message in case of error
    
    * Deleting unnecessary comments
    
    * Adding a comment in token
    
    * Fixing slack_module bug
    
    * Removing token

---
+++ invoicing_app/slack_module.py
@@ -0,0 +1,21 @@
+import requests
+
+SLACK_API_ENDPOINT_POST = 'https://slack.com/api/chat.postMessage'
+
+
+channels = {
+    '#invoicing_arg_brl': 'C014Z5CFCHW',
+}
+
+
+class SlackConnection():
+    def __init__(self, token):
+        self.slack_api_token = token
+
+    def post_message(self, channel, message):
+        data = {
+            'token': self.slack_api_token,
+            'channel': channels[channel],
+            'text': message
+        }
+        response = requests.post(url=SLACK_API_ENDPOINT_POST, data=data)
